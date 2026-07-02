be4d812d889365aa07bfdb8325c15887146d5740
Olumide Adekunle <116625009+olumideadekunle@users.noreply.github.com>
2026-07-02 09:52:50 +0100

    Refactor send_to_address call with additional parameters

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,7 +7,7 @@ use std::io::Write;
 fn main() -> bitcoincore_rpc::Result<()> {
     let rpc_url = "http://127.0.0.1:18443";
     let auth = Auth::UserPass("alice".to_string(), "password".to_string());
-    
+
     let miner_rpc = Client::new(format!("{}/wallet/Miner", rpc_url).as_str(), auth.clone())?;
     let trader_rpc = Client::new(format!("{}/wallet/Trader", rpc_url).as_str(), auth.clone())?;
 
@@ -16,9 +16,14 @@ fn main() -> bitcoincore_rpc::Result<()> {
 
     let trader_addr = trader_rpc.get_new_address(Some("Received"), None)?.assume_checked();
     let txid = miner_rpc.send_to_address(
-        &trader_addr, 
-        Amount::from_btc(20.0).unwrap(), 
-        None, None, None, None, None, None
+        &trader_addr,
+        Amount::from_btc(20.0).unwrap(),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
     )?;
 
     let tx = miner_rpc.get_transaction(&txid, Some(true))?;
